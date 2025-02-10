09c73321116fffea39707be536ae416f6875eec4
aboumediene <abdelkader.boumediene@gmail.com>
2025-02-10 11:41:13 +0100

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -2,7 +2,7 @@
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'wordpress_user');
 define('DB_PASSWORD', 'wordpress_password');
-define('DB_HOST', 'db'); // "db" est le nom du service MySQL dans docker-compose.yml
+define('DB_HOST', 'mariadb'); // "db" est le nom du service MySQL dans docker-compose.yml
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
