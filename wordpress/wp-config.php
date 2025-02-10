0414368dda8017bb2d5ca82260b0b83fd6cb779c
aboumediene <abdelkader.boumediene@gmail.com>
2025-02-10 10:57:08 +0100

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,21 +1,15 @@
-<?php
+// wordpress/wp-config.php
 define('DB_NAME', 'wordpress');
 define('DB_USER', 'user');
 define('DB_PASSWORD', 'password');
 define('DB_HOST', 'db'); // "db" est le nom du service MySQL dans docker-compose.yml
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
 
-define('WP_HOME', 'http://localhost:8080');
-define('WP_SITEURL', 'http://localhost:8080');
 
 
-$table_prefix = 'wp_';
+// Préfixe des tables : changez si besoin
+$table_prefix  = 'wp_';
 
-// define('WP_DEBUG', false);
-define('WP_DEBUG', true);
-define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
-
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/');
-
-require_once ABSPATH . 'wp-settings.php';
+// Debug mode : à désactiver en production
+define('WP_DEBUG', false);
