0e359f010f0d8e517edfb9180022c97edbeabd0c
aboumediene <abdelkader.boumediene@gmail.com>
2025-02-10 20:18:55 +0100

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,13 +1,32 @@
 // wordpress/wp-config.php
-define('DB_NAME', 'wordpressdb');
-define('DB_USER', 'wordpress_user');
-define('DB_PASSWORD', 'wordpress_password');
-define('DB_HOST', 'mariadb'); // "db" est le nom du service MySQL dans docker-compose.yml
+define('DB_NAME', 'worddb');
+define('DB_USER', 'moi');
+define('DB_PASSWORD', 'aacher');
+define('DB_HOST', 'mariadb'); // Correspond au service mariadb dans docker-compose
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
+
+
 
 // Préfixe des tables : changez si besoin
 $table_prefix  = 'wp_';
 
 // Debug mode : à désactiver en production
 define('WP_DEBUG', false);
+
+
+
+// ---------------------- my -------------------
+// // wordpress/wp-config.php
+// define('DB_NAME', 'wordpressdb');
+// define('DB_USER', 'wordpress_user');
+// define('DB_PASSWORD', 'wordpress_password');
+// define('DB_HOST', 'mariadb'); // "db" est le nom du service MySQL dans docker-compose.yml
+// define('DB_CHARSET', 'utf8');
+// define('DB_COLLATE', '');
+
+// // Préfixe des tables : changez si besoin
+// $table_prefix  = 'wp_';
+
+// // Debug mode : à désactiver en production
+// define('WP_DEBUG', false);
