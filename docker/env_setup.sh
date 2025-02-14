8ef355309989a9c15158b179e1e02678d1118075
aboumediene <abdelkader.boumediene@gmail.com>
2025-02-14 19:14:47 +0100

    Update env_setup.sh

--- docker/env_setup.sh
+++ docker/env_setup.sh
@@ -2,14 +2,10 @@
 
 echo "remove installed env ..........................."
 docker-compose down -v
-echo "remove created image ..........................."
-docker rmi wordpress:latest
-docker rmi web-server-project_nginx       
-docker rmi web-server-project_wordpress
+echo "remove created images ..........................."
+docker rmi wordpress:latest web-server-project_nginx web-server-project_wordpress
 docker rmi web-server-project_mariadb  
-docker rmi nginx                       
-docker rmi mariadb                     
-docker rmi wordpress  
+docker rmi nginx mariadb wordpress docker_nginx docker_mariadb docker_wordpress 
 echo "update scripts..........................."
 git pull
 echo "Install env ..........................."
